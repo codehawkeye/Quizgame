db063d6ef5d16fe2e4efcc11973732b016189210
Mike Carter <elite.mikecarter@gmail.com>
2020-12-22 16:35:28 -0500

    asigned quiz varibles and creating question array

---
+++ game.js
@@ -0,0 +1,20 @@
+const question = document.querySelector('#question');
+const choices = Array.from(document.querySelectorall('.choice-text'));
+const progressText = document.querySelector('#ProgressText');
+const ScoreText = document.querySelector('#score');
+const ProgressBarfull = document.querySelector('#ProgressBarFull');
+
+let currentQuestion = { }
+let acceptingAnswers = true
+let score = 0
+let questionCounter = 0 
+let avalibileQuestion = []
+
+let question = {
+
+
+
+    
+}
+
+
