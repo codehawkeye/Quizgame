7523d2b5751246b68eb5ca6879947ae8f28ebd32
Mike Carter <elite.mikecarter@gmail.com>
2020-12-24 12:16:57 -0500

    moved files into main folder

---
+++ end.js
@@ -0,0 +1,39 @@
+const username = document.querySelector('#username')
+const saveScoreBTN = document.querySelector('#savScoreBtn')
+const finalScore = document.querySelector('#finalScore')
+const mostRecentScore = localStorage.getItem('mostRecentScore')
+
+const highScore = JSON.parse(localStorage.getItem('highScore')) || []
+
+const MAX_HIGH_SCORES = 5
+
+finalScore.innerText = mostRecentScore
+
+
+
+username.addEventListener('keyup', () => {
+    saveScoreBTN.disabled = !username.nodeValue
+})
+
+saveHighScore = e => {
+    e.preventDefault()
+
+
+const score = {
+    score: mostRecentScore,
+        name: username.value
+}
+
+highScore.push(score)
+
+highScore.sort((a, b) => {
+    return b.score -a.score
+})
+
+highScore.splice(5)
+
+localStorage.setItem('highScore', JSON.stringify(highScore))
+window.location.assign('/')
+
+
+}
