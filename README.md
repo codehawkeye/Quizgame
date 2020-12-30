8d5097b793138562109823c35e9c8cf72111da15
Mike Carter <elite.mikecarter@gmail.com>
2020-12-30 13:11:16 -0500

    changed hard code HTMl to dynamic

--- README.md
+++ README.md
@@ -20,7 +20,7 @@ SO THAT I can gauge my progress compared to my peers
 
 ```
 GIVEN I am taking a code quiz
-WHEN I click the start button
+x-- WHEN I click the start button
 THEN a timer starts and I am presented with a question
 WHEN I answer a question
 THEN I am presented with another question
--- game.js
+++ game.js
@@ -1,23 +1,19 @@
 const question = document.querySelector('#question');
-const choices = Array.from(document.querySelectorall('.choice-text'));
 const progressText = document.querySelector('#progressText');
 const ScoreText = document.querySelector('#score');
 const progressBarfull = document.querySelector('#ProgressBarFull');
 
-let currentQuestion = {}
 let acceptingAnswers = true
 let score = 0
 let questionCounter = 0 
 let avalibileQuestion = []
+let choiceDiv = document.querySelector('#choices');
 
 let questions = [
     {
         question: 'Mario was originally known.......',
-        choice1: "Jumpman",
-        choice2: "Super Plumber",
-        choice3: "Dashing Dave",
-        choice4: "Fixit Felix",
-        answer: 1,    
+        choices: ["jumpman", "Superman"," Dashing Dave", "Fixit Felix"],        
+         answer: "jumpman"    
     },
     {
         question: " Mario was originally a.........",
@@ -79,224 +75,224 @@ let questions = [
         choice3: "The Gamer",
         choice4: "Foot Loose",
         answer: 2,   
-    },
-    {
-        question: 'what is...........',
-        choice: "A",
-        choice: "B",
-        choice: "c",
-        choice: "D",
-        answer: "A",    
-    },
+    }
+    // {
+    //     question: 'what is...........',
+    //     choice: "A",
+    //     choice: "B",
+    //     choice: "c",
+    //     choice: "D",
+    //     answer: "A",    
+    // },
 
-    {
-        question: "what is...........",
-        choice: "A",
-        choice: "B",
-        choice: "c",
-        choice: "D",
-        answer: "A",   
-    },
+    // {
+    //     question: "what is...........",
+    //     choice: "A",
+    //     choice: "B",
+    //     choice: "c",
+    //     choice: "D",
+    //     answer: "A",   
+    // },
 
-    {
-        question: "what is...........",
-        choice: "A",
-        choice: "B",
-        choice: "c",
-        choice: "D",
-        answer: "A",    
-    },
+    // {
+    //     question: "what is...........",
+    //     choice: "A",
+    //     choice: "B",
+    //     choice: "c",
+    //     choice: "D",
+    //     answer: "A",    
+    // },
 
-    {
-        question: "what is...........",
-        choice: "A",
-        choice: "B",
-        choice: "c",
-        choice: "D",
-        answer: "A",   
-    },
-
-
-    {
-        question: 'what is...........',
-        choice: "A",
-        choice: "B",
-        choice: "c",
-        choice: "D",
-        answer: "A",    
-    },
-
-    {
-        question: "what is...........",
-        choice: "A",
-        choice: "B",
-        choice: "c",
-        choice: "D",
-        answer: "A",   
-    },
-
-    {
-        question: "what is...........",
-        choice: "A",
-        choice: "B",
-        choice: "c",
-        choice: "D",
-        answer: "A",    
-    },
+    // {
+    //     question: "what is...........",
+    //     choice: "A",
+    //     choice: "B",
+    //     choice: "c",
+    //     choice: "D",
+    //     answer: "A",   
+    // },
 
 
-    {
-        question: "what is...........",
-        choice: "A",
-        choice: "B",
-        choice: "c",
-        choice: "D",
-        answer: "A",
+    // {
+    //     question: 'what is...........',
+    //     choice: "A",
+    //     choice: "B",
+    //     choice: "c",
+    //     choice: "D",
+    //     answer: "A",    
+    // },
+
+    // {
+    //     question: "what is...........",
+    //     choice: "A",
+    //     choice: "B",
+    //     choice: "c",
+    //     choice: "D",
+    //     answer: "A",   
+    // },
+
+    // {
+    //     question: "what is...........",
+    //     choice: "A",
+    //     choice: "B",
+    //     choice: "c",
+    //     choice: "D",
+    //     answer: "A",    
+    // },
+
+
+    // {
+    //     question: "what is...........",
+    //     choice: "A",
+    //     choice: "B",
+    //     choice: "c",
+    //     choice: "D",
+    //     answer: "A",
             
-    },
+    // },
     
-    {
-        question: 'what is...........',
-        choice: "A",
-        choice: "B",
-        choice: "c",
-        choice: "D",
-        answer: "A",    
-    },
-    {
-        question: "what is...........",
-        choice: "A",
-        choice: "B",
-        choice: "c",
-        choice: "D",
-        answer: "A",   
-    },
-    {
-        question: "what is...........",
-        choice: "A",
-        choice: "B",
-        choice: "c",
-        choice: "D",
-        answer: "A",    
-    },
+    // {
+    //     question: 'what is...........',
+    //     choice: "A",
+    //     choice: "B",
+    //     choice: "c",
+    //     choice: "D",
+    //     answer: "A",    
+    // },
+    // {
+    //     question: "what is...........",
+    //     choice: "A",
+    //     choice: "B",
+    //     choice: "c",
+    //     choice: "D",
+    //     answer: "A",   
+    // },
+    // {
+    //     question: "what is...........",
+    //     choice: "A",
+    //     choice: "B",
+    //     choice: "c",
+    //     choice: "D",
+    //     answer: "A",    
+    // },
 
-    {
-        question: "what is...........",
-        choice: "A",
-        choice: "B",
-        choice: "c",
-        choice: "D",
-        answer: "A",   
-    },
+    // {
+    //     question: "what is...........",
+    //     choice: "A",
+    //     choice: "B",
+    //     choice: "c",
+    //     choice: "D",
+    //     answer: "A",   
+    // },
 
-    {
-        question: 'what is...........',
-        choice: "A",
-        choice: "B",
-        choice: "c",
-        choice: "D",
-        answer: "A",    
-    },
+    // {
+    //     question: 'what is...........',
+    //     choice: "A",
+    //     choice: "B",
+    //     choice: "c",
+    //     choice: "D",
+    //     answer: "A",    
+    // },
 
-    {
-        question: "what is...........",
-        choice: "A",
-        choice: "B",
-        choice: "c",
-        choice: "D",
-        answer: "A",   
-    },
+    // {
+    //     question: "what is...........",
+    //     choice: "A",
+    //     choice: "B",
+    //     choice: "c",
+    //     choice: "D",
+    //     answer: "A",   
+    // },
 
-    {
-        question: "what is...........",
-        choice: "A",
-        choice: "B",
-        choice: "c",
-        choice: "D",
-        answer: "A",    
-    },
+    // {
+    //     question: "what is...........",
+    //     choice: "A",
+    //     choice: "B",
+    //     choice: "c",
+    //     choice: "D",
+    //     answer: "A",    
+    // },
 
-    {
-        question: "what is...........",
-        choice: "A",
-        choice: "B",
-        choice: "c",
-        choice: "D",
-        answer: "A",   
-    },
-    {
-        question: 'what is...........',
-        choice: "A",
-        choice: "B",
-        choice: "c",
-        choice: "D",
-        answer: "A",    
-    },
+    // {
+    //     question: "what is...........",
+    //     choice: "A",
+    //     choice: "B",
+    //     choice: "c",
+    //     choice: "D",
+    //     answer: "A",   
+    // },
+    // {
+    //     question: 'what is...........',
+    //     choice: "A",
+    //     choice: "B",
+    //     choice: "c",
+    //     choice: "D",
+    //     answer: "A",    
+    // },
 
-    {
-        question: "what is...........",
-        choice: "A",
-        choice: "B",
-        choice: "c",
-        choice: "D",
-        answer: "A",   
-    },
+    // {
+    //     question: "what is...........",
+    //     choice: "A",
+    //     choice: "B",
+    //     choice: "c",
+    //     choice: "D",
+    //     answer: "A",   
+    // },
 
-    {
-        question: "what is...........",
-        choice: "A",
-        choice: "B",
-        choice: "c",
-        choice: "D",
-        answer: "A",    
-    },
+    // {
+    //     question: "what is...........",
+    //     choice: "A",
+    //     choice: "B",
+    //     choice: "c",
+    //     choice: "D",
+    //     answer: "A",    
+    // },
 
-    {
-        question: "what is...........",
-        choice: "A",
-        choice: "B",
-        choice: "c",
-        choice: "D",
-        answer: "A",   
-    },
+    // {
+    //     question: "what is...........",
+    //     choice: "A",
+    //     choice: "B",
+    //     choice: "c",
+    //     choice: "D",
+    //     answer: "A",   
+    // },
 
 
-    {
-        question: 'what is...........',
-        choice: "A",
-        choice: "B",
-        choice: "c",
-        choice: "D",
-        answer: "A",    
-    },
+    // {
+    //     question: 'what is...........',
+    //     choice: "A",
+    //     choice: "B",
+    //     choice: "c",
+    //     choice: "D",
+    //     answer: "A",    
+    // },
 
-    {
-        question: "what is...........",
-        choice: "A",
-        choice: "B",
-        choice: "c",
-        choice: "D",
-        answer: "A",   
-    },
+    // {
+    //     question: "what is...........",
+    //     choice: "A",
+    //     choice: "B",
+    //     choice: "c",
+    //     choice: "D",
+    //     answer: "A",   
+    // },
 
-    {
-        question: "what is...........",
-        choice: "A",
-        choice: "B",
-        choice: "c",
-        choice: "D",
-        answer: "A",    
-    },
+    // {
+    //     question: "what is...........",
+    //     choice: "A",
+    //     choice: "B",
+    //     choice: "c",
+    //     choice: "D",
+    //     answer: "A",    
+    // },
     
-    {
-        question: "what is...........",
-        choice: "A",
-        choice: "B",
-        choice: "c",
-        choice: "D",
-        answer: "A",
+    // {
+    //     question: "what is...........",
+    //     choice: "A",
+    //     choice: "B",
+    //     choice: "c",
+    //     choice: "D",
+    //     answer: "A",
             
-    }   
+    // }   
 
 
 
@@ -307,61 +303,71 @@ const MAX_QUESTIONS = 4
 startGame = () => {
     questionCounter = 0
     score = 0
-    avalibileQuestions = [...questions]
     getNewQuestion()
 }
 
 getNewQuestion = () => {
-    if (avalibileQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
-        localStorage.setItem('mostRecentScore', score)
-
-        return window.location.assign('/end.html')
-
-    }
-    questionCounter++
-    progressText.innerText = 'Question ${questionCounter} of ${MAX_QESTIONS}'
-    ProgressBarfull.style.width = '${(questionCounter/MAX_QUESTIONS) * 100}%'
-    
-
-    const questionsIndex = Math.floor(Math.random() * avalibileQuestions.length)
-    currentQuestion = avalibileQuestions[questionsIndex]
-    question.innerText = currentQuestion.question
-
-
-    choices.forEach(choice => {
-        const number = choice.dataset['number']
-        choice.innerText = currentQuestion['choice' + number]
+    var currentQuestion = questions[questionCounter] 
+    question.textContent = currentQuestion.question
+    currentQuestion.choices.forEach(function (choice) {
+        var choiceEL = document.createElement("button")
+        choiceEL.setAttribute("value",choice)
+        choiceEL.textContent = choice
+        // 
+        choiceEL.addEventListener('click',)
+        choiceDiv.appendChild(choiceEL)
         
     })
+        // if (avalibileQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
+    //     localStorage.setItem('mostRecentScore', score)
 
-    avalibileQuestions.splice(questionsIndex, 1)
+    //     return window.location.assign('/end.html')
 
-    acceptingAnswers = true
+    // }
+    // questionCounter++
+    // progressText.innerText = 'Question ${questionCounter} of ${MAX_QESTIONS}'
+    // ProgressBarfull.style.width = '${(questionCounter/MAX_QUESTIONS) * 100}%'
+    
+
+    // const questionsIndex = Math.floor(Math.random() * avalibileQuestions.length)
+    // currentQuestion = avalibileQuestions[questionsIndex]
+    // question.innerText = currentQuestion.question
+
+
+    // choices.forEach(choice => {
+    //     const number = choice.dataset['number']
+    //     choice.innerText = currentQuestion['choice' + number]
+        
+    // })
+
+    // avalibileQuestions.splice(questionsIndex, 1)
+
+    // acceptingAnswers = true
     
 }   
 
-choices.forEach(choice => {
-    choice.addEventListener('click', e => {
-        if(!acceptingAnswers) return
+// choices.forEach(choice => {
+//     choice.addEventListener('click', e => {
+//         if(!acceptingAnswers) return
         
-        acceptingAnswers = false
-        const selectedChoice = e.target
-        const selectedAnswer = selectedChoice.dataset['number']
+//         acceptingAnswers = false
+//         const selectedChoice = e.target
+//         const selectedAnswer = selectedChoice.dataset['number']
         
-        let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect'
-        if(classToApply === 'correct') {
-            incrementScore(SCORE_POINTS)
-        }
-        selectedChoice.parentElement.classList.add(classToApply)
+//         let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect'
+//         if(classToApply === 'correct') {
+//             incrementScore(SCORE_POINTS)
+//         }
+//         selectedChoice.parentElement.classList.add(classToApply)
 
 
-        setTimeout(() => {
-            selectedChoice.parentElement.classList.remove(classToApply)
-            getNewQuestion()
+//         setTimeout(() => {
+//             selectedChoice.parentElement.classList.remove(classToApply)
+//             getNewQuestion()
 
-        }, 1000)
-    })
-})
+//         }, 1000)
+//     })
+// })
 
 
 incrementScore = num => {
